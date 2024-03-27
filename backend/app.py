652d4401567fd15e4ecc8b7e18230f386275e2c7
Rafay-Zubair <zubairgillrafay@gmail.com>
2024-03-27 20:47:07 +0500

    initial commit

---
+++ backend/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+import os
+
+app = Flask(__name__)
+
+# Connection with MongoDB
+db_client = MongoClient(os.environ.get('HOST_URL'), int(os.environ.get('DB_PORT')))
+# Database
+db = db_client.user_data
+# Collection
+doc_collection = db.users
+
+# if doc_collection.count_documents({}) == 0:
+# 	# When doctors collection is empty
+# 	doctors = [
+# 		{ 'id': "1",'firstName': "Muhammad Ali", 'lastName': "Kahoot", 'speciality' : "DevOps"  },
+# 		{ 'id': "2",'firstName': "Good", 'lastName': "Doctor",'speciality' : "Test"  }
+# 	]	
+# 	for doctor in doctors:
+# 		doc_collection.insert_one(doctor)
+
+@app.route('/hello')
+def hello():
+	greeting = "Hello world!"
+	return greeting
+
+@app.route('/users', methods=["GET"])
+def getAllUsers():
+	data = list(doc_collection.find())
+	for d in data:
+		d.pop("_id")
+	return jsonify(data)
+
+@app.route('/addUser', method=["POST"])
+def insertUser():
+  userName = request.form['username']
+  email = request.form['email']
+  
+  doc_collection.insert_one({ 'uesrname':userName, 'email':email })
+  
+  return jsonify({ 'uesrname':userName, 'email':email })
+
+if __name__ == "__main__":
+	app.run(host="0.0.0.0",port=9090)
+
