9ff27a9186918ffb71e2a0e86344e901c9ce682c
Rafay Zubair Gill <115183292+Rafay-Zubair@users.noreply.github.com>
2024-03-27 17:24:30 +0000

    front end

---
+++ frontend/frontend/src/App.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [formData, setFormData] = useState({ username: '', email: '' });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://${process.env.BACKEND_SERVICE_URL}/user', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      console.log('Response from server:', data);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          name="username"
+          value={formData.username}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <br />
+      <label>
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          required
+        />
+      </label>
+      <br />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
+
+export default App;
